06d34ba42015dfd48ee05a1e4fe3ebad3fcdf7c9
George Lucas <georgelucasgit@gmail.com>
2023-08-03 17:48:49 -0300

    Estilização e conclusão da questão

--- questao01.php
+++ questao01.php
@@ -4,15 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerador de Elementos HTML</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 </head>
 <html>
 <body>
-    <h1>Gerador de Elementos HTML e respectivos codigos-fonte</h1>
-    <form action="" method="get" id="formulario-questoes">
+    <h1>Gerador de elementos HTML e respectivos códigos</h1>
+    <form action="" method="get" id="formulario-questoes" >
         <fieldset>
-            <legend>Digite seu numero:</legend>
+            <legend>Geração de elementos</legend>
             <label for="totalElementos">Total de elementos:</label>
-            <input type="number" name="totalElementos" id="totalElementos" min="1" max="15" value="1">
+            <input type="number" class="input-elementos" name="totalElementos" id="totalElementos" min="1" max="15" value="1">
             <span> (1 a 15)</span><br><br>
             
             <label for="texto">Texto</label>
@@ -41,17 +45,72 @@
         if ($totalElementos >= 1 && $totalElementos <= 15) {
             if ($elementoSelecionado == "texto") {
                 for ($i = 1; $i <= $totalElementos; $i++) {
-                    echo "<br> <input type=\"text\" name=\"text$i\" id=\"text$i\"><br>";
+                    echo "<br><input type=\"text\" class=\"input-exemplo\" name=\"text$i\" id=\"text$i\">";
                 }
                 echo "<br>";
                 for ($i = 1; $i <= $totalElementos; $i++) {
-                    echo "&lt;input type=\"text\" name=\"text$i\" id=\"text$i\"&gt;&lt;br&gt;<br>";
+                    echo "<p>&lt;input type=\"text\" name=\"text$i\" id=\"text$i\"&gt;&lt;br&gt;</p>";
+                }
+            } else if ($elementoSelecionado == "senha") {
+                for ($i = 1; $i <= $totalElementos; $i++) {
+                    echo " <br><input type=\"password\" class=\"input-exemplo\" name=\"password$i\" id=\"password$i\">";
+                }
+                echo "<br>";
+                for ($i = 1; $i <= $totalElementos; $i++) {
+                    echo "<p>&lt;input type=\"password\" name=\"text$i\" id=\"text$i\"&gt;&lt;br&gt;</p>";
+                }
+            } else if ($elementoSelecionado == "botao") {
+                for ($i = 1; $i <= $totalElementos; $i++) {
+                    echo "<br><input type=\"button\" class=\"input-botao\" value=\"Botão $i\">";
+                }
+                echo "<br>";
+                for ($i = 1; $i <= $totalElementos; $i++) {
+                    echo "<p>&lt;input type=\"button\" value=\"Botão $i\"&gt;&lt;br&gt;</p>";
+                }
+            } else if ($elementoSelecionado == "radio") {
+                for ($i = 1; $i <= $totalElementos; $i++) {
+                    echo "<br><input type=\"radio\" name=\"radio\" id=\"radio$i\">";
+                    echo "<label for=\"radio$i\">Radio $i</label>";
+                }
+                echo "<br>";
+                for ($i = 1; $i <= $totalElementos; $i++) {
+                    echo "<p>&lt;input type=\"radio\" name=\"radio\" id=\"radio$i\"&gt;</p>";
+                    echo "<p>&lt;label for=\"radio$i\"&gt;Radio $i&lt;/label&gt;&lt;br&gt;</p>";
+                }
+            } else if ($elementoSelecionado == "caixaselecao") {
+                for ($i = 1; $i <= $totalElementos; $i++) {
+                    echo "<br><input type=\"checkbox\" name=\"checkbox$i\" id=\"checkbox$i\">";
+                    echo "<label for=\"checkbox$i\">Caixa de Seleção $i</label>";
+                }
+                echo "<br>";
+                for ($i = 1; $i <= $totalElementos; $i++) {
+                    echo "<br><p>&lt;input type=\"checkbox\" name=\"checkbox$i\" id=\"checkbox$i\"&gt;";
+                    echo "<p>&lt;label for=\"checkbox$i\"&gt;Caixa de Seleção $i&lt;/label&gt;&lt;br&gt;</p>";
+                }
+            } else if ($elementoSelecionado == "faixa") {
+                $valuefaixa = 5;
+                for ($i = 1; $i <= $totalElementos; $i++) {
+                    echo "<br><input type=\"range\" name=\"range$i\" id=\"range$i\" min=\"0\" max =\"100\" value=\"$valuefaixa\">";
+                    echo "<label for=\"range$i\">Faixa $i</label>";
+                    $valuefaixa = $valuefaixa + 5;
+                }
+                echo "<br>";
+                $valuefaixa = 5;
+                for ($i = 1; $i <= $totalElementos; $i++) {
+                    echo "<p>&lt;input type=\"range\" name=\"range$i\" id=\"range$i\" min=\"0\" max=\"100\" value=\"$valuefaixa\"&gt;</p>";
+                    echo "<p>&lt;label for=\"range$i\"&gt;Faixa $i&lt;/label&gt;&lt;br&gt;</p>";
+                    $valuefaixa = $valuefaixa + 5;
                 }
             }
         } else {
-            echo "Quantidade invalida. O numero de elementos deve estar de 01 a 15.";
+            echo "<br>";
+            echo "Quantidade inválida. O numero de elementos deve estar de 1 a 15.";
         }
         ?>
+         <div class="center">
+        <a href="../index.php">Página Inicial</a>
+    </div>
     </form>
+   
 </body>
 </html>
