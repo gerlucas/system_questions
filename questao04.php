982aef10f53f236c7166f5ff59d56a7bf734a7c9
George Lucas <georgelucasgit@gmail.com>
2023-08-04 13:50:18 -0300

    Resolvendo bugs da tabela e do input

--- questao04.php
+++ questao04.php
@@ -21,7 +21,7 @@
             <fieldset>
                 <legend>Parâmetros</legend>
                 <label for="aporteInicial">Aporte inicial (R$):</label>
-                <input type="number" class="input-elementos" name="aporteInicial" id="aporteInicial" min="1" max="99999999.99" value="500">
+                <input type="number" class="input-elementos" name="aporteInicial" id="aporteInicial" min="1" max="999999.99" step="0.01" value="500">
                 <span> [até R$999.999,99]</span><br>
 
                 <label for="periodo">Período (meses):</label>
@@ -29,14 +29,15 @@
                 <span> [de 1 a 480 meses]</span><br>
 
                 <label for="rendimentoMensal">Rendimento mensal (%):</label>
-                <input type="number" class="input-elementos" name="rendimentoMensal" id="rendimentoMensal" min="0,1" max="20" value="0.7">
+                <input type="number" class="input-elementos" name="rendimentoMensal" id="rendimentoMensal" min="0.1" max="20" step="0.01" value="0.7">
                 <span> [até 20%]</span><br>
+
 
                 <label for="aporteMensal">Aporte mensal:</label>
                 <input type="number" class="input-elementos" name="aporteMensal" id="aporteMensal" min="1" max="99999999.99" value="350">
                 <span> [até R$999.999,99]</span><br>
 
-                <input type="submit" value="Processar">
+                <input type="submit" class="input-botao" value="Processar">
             </fieldset>
 
             <?php
@@ -44,7 +45,7 @@
             $periodo = $_GET["periodo"] ?? 12;
             $rendimentoMensal = $_GET["rendimentoMensal"] ?? 0.7;
             $aporteMensal = $_GET["aporteMensal"] ?? 350;
-            
+
             function calcularRendimento($aporteInicial, $periodo, $rendimentoMensal, $aporteMensal)
             {
                 $total = $aporteInicial;
