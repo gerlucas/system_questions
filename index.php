c57f94a56ba87b751fa43a6adc9992e0ea172543
George Lucas <georgelucasgit@gmail.com>
2023-08-05 16:33:56 -0300

    Finalizando estilização e pagina home

--- index.php
+++ index.php
@@ -2,26 +2,37 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <?php 
-    $paginas = [
-    'questao01.php' =>  'Questão 01',
-    'questao02.php' =>  'Questão 02',
-    'questao03-1.php' =>  'Questão 03',
-    'questao04.php' =>  'Questão 04',
-    ];
+    <section>
+        <div class="circle"></div>
+        <div class="content">
+            <div class="text">
+                <h2>Sistema de<br><span>Questões</span></h2>
+                <p>O nosso sistema é um projeto educacional para estudo em PHP.</p>
+                <?php
+                $paginas = [
+                    'questao01.php' => 'Acesse a questão 01',
+                    'questao02.php' => 'Acesse a questão 02',
+                    'questao03-1.php' => 'Acesse a questão 03',
+                    'questao04.php' => 'Acesse a questão 04',
+                ];
 
-    $link = '<li><a href="%s">%s</a></li>';
+                $link = '<a href="%s">%s</a>';
 
-    foreach ($paginas as $pagina => $titulo){
-        printf($link, $pagina, $titulo);
-
-    }
-    ?>
+                foreach ($paginas as $pagina => $titulo) {
+                    printf($link, $pagina, $titulo);
+                }
+                ?>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
