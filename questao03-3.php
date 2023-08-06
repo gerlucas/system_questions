012d32017002bc0dc803fee47bf4475bc45e64fc
gabriel <130569884+GabriSF@users.noreply.github.com>
2023-08-06 02:19:55 +0000

    Finalizando enunciado da questão 03

--- questao03-3.php
+++ questao03-3.php
@@ -19,14 +19,14 @@
 
 
     <main>
-        <h2>Q3. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum pariatur ducimus ex rem reprehenderit!</h2>
+        <h2>Q3. Qual desses nomes são mais usados na aula de programação?</h2>
         <div class="alternativas">
-            <p><a href="./questao03-4.php?resp1=<?= $_GET['resp1'] ?>&resp2=<?= $_GET['resp2'] ?>&resp3=1" class="alternativa alternativa-1 correta">A&#41; Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum pariatur ducimus ex rem reprehenderit!</a></p>
-            <p><a href="./questao03-4.php?resp1=<?= $_GET['resp1'] ?>&resp2=<?= $_GET['resp2'] ?>&resp3=2" class="alternativa alternativa-2">B&#41; Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum pariatur ducimus ex rem reprehenderit!</a></p>
-            <p><a href="./questao03-4.php?resp1=<?= $_GET['resp1'] ?>&resp2=<?= $_GET['resp2'] ?>&resp3=3" class="alternativa alternativa-3">C&#41; Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum pariatur ducimus ex rem reprehenderit!</a></p>
-            <p><a href="./questao03-4.php?resp1=<?= $_GET['resp1'] ?>&resp2=<?= $_GET['resp2'] ?>&resp3=4" class="alternativa alternativa-4">D&#41; Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum pariatur ducimus ex rem reprehenderit!</a></p>
+            <p><a href="./questao03-4.php?resp1=<?= $_GET['resp1'] ?>&resp2=<?= $_GET['resp2'] ?>&resp3=1" class="alternativa alternativa-1">A&#41; Beatriz Yana.</a></p>
+            <p><a href="./questao03-4.php?resp1=<?= $_GET['resp1'] ?>&resp2=<?= $_GET['resp2'] ?>&resp3=2" class="alternativa alternativa-2">B&#41; asdf123.</a></p>
+            <p><a href="./questao03-4.php?resp1=<?= $_GET['resp1'] ?>&resp2=<?= $_GET['resp2'] ?>&resp3=3" class="alternativa alternativa-3">C&#41; Abóbora</a></p>
+            <p><a href="./questao03-4.php?resp1=<?= $_GET['resp1'] ?>&resp2=<?= $_GET['resp2'] ?>&resp3=4" class="alternativa alternativa-4">D&#41; Carlos Xavier.</a></p>
         </div>
-        <p><a href="./questao03-4.php?resp1=<?= $_GET['resp1'] ?>&resp2=<?= $_GET['resp2'] ?>&resp3=5" class="alternativa alternativa-5">E&#41; Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum pariatur ducimus ex rem reprehenderit!</a></p>
+        <p><a href="./questao03-4.php?resp1=<?= $_GET['resp1'] ?>&resp2=<?= $_GET['resp2'] ?>&resp3=5" class="alternativa alternativa-5 correta">E&#41; Ana Zaira.</a></p>
     </main>
 
 
